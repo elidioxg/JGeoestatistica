8cd68ef7169a22911aa5e73d42add5ceb82ef8da
elidioxg <elidioxg@gmail.com>
2016-02-24 14:36:34 -0300

    Covariance modified

--- src/geoestatistica/Statistics/Covariance.java
+++ src/geoestatistica/Statistics/Covariance.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Covariance {
     
-    public double covariance(ArrayList<Double> array1, ArrayList<Double> array2,
-            double average1, double average2) throws Exception {
+    public double covariance(ArrayList<Double> array1, 
+            ArrayList<Double> array2) throws Exception {
         double result = 0.;
         if(array1.size()!=array2.size()){
             throw new Exception("The arrays must have same size");
@@ -19,4 +19,17 @@ public class Covariance {
         }               
         return result/array1.size();
     }    
+    
+    public double covariance(ArrayList<Double> array1, ArrayList<Double> array2,
+            double average1, double average2) throws Exception {
+        double result = 0.;
+        if(array1.size()!=array2.size()){
+            throw new Exception("The arrays must have same size");
+        } else {
+            for(int i = 0; i <array1.size()-1; i++){
+                result += (array1.get(i) - average1)*(array2.get(i) - average2);
+            }
+        }               
+        return result/array1.size();
+    }
 }
