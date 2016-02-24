53fc45bda5b61ae2c9b88ca205a449d02837856a
elidioxg <elidioxg@gmail.com>
2016-02-24 15:20:25 -0300

    Added Pearson Coefficient

--- src/geoestatistica/Statistics/Correlation.java
+++ src/geoestatistica/Statistics/Correlation.java
@@ -1,7 +1,17 @@
 package geoestatistica.Statistics;
 
+import java.util.ArrayList;
+
 public class Correlation {
     
-    //TODO: Pearson coefficient
+    public double pearsonCoeff(ArrayList<Double> array1, 
+            ArrayList<Double> array2) throws Exception{        
+        Variance variance = new Variance();
+        Covariance covariance = new Covariance();
+        double var1 = variance.variance(array1);
+        double var2 = variance.variance(array2);
+        double cov = covariance.covariance(array1, array2);
+        return cov/(Math.sqrt(var1*var2));        
+    }
     
 }
